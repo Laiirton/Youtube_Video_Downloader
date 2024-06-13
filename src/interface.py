557c335d3c2f12cb1f8c5d4248682aecfe9002e3
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-13 02:17:27 -0300

    chore: Update canvas size and add title contour in interface.py

--- src/interface.py
+++ src/interface.py
@@ -4,26 +4,32 @@ from PIL import Image, ImageTk
 
 # Configuração da janela principal
 app = ctk.CTk()
-app.geometry("1000x600")
+app.geometry("800x600")
 app.title("YouTube Video Downloader")
+app.resizable(False, False)
 
 # Carregar e configurar a imagem de fundo
 bg_image = Image.open("./src/assets/background.jpg")
+bg_image = bg_image.resize((800, 600), Image.LANCZOS)
 bg_image = ImageTk.PhotoImage(bg_image)
-canvas = tk.Canvas(app, width=1000, height=600)
+
+canvas = tk.Canvas(app, width=800, height=600, highlightthickness=0)  # Remover borda do canvas
 canvas.pack(fill="both", expand=True)
 canvas.create_image(0, 0, image=bg_image, anchor="nw")
 
-# Adicionar título diretamente no canvas
-canvas.create_text(200, 50, text="YouTube Downloader", font=("Helvetica", 24), fill="white")
+# Cria um contorno suavizado para o texto
+offsets = [(x, y) for x in range(-1, 2) for y in range(-1, 2)]
+for x, y in offsets: 
+    # Adicionar título diretamente no canvas
+    canvas.create_text(200 + x, 50 + y, text="YouTube Downloader", font=("Helvetica", 24,), fill="white", anchor="center")
 
 # Função de download de vídeo (placeholder)
 def download_video():
     print("Downloading video...")
 
 # Adicionar botão de download
-button = tk.Button(app, text="Download Video", command=download_video)
-button_window = canvas.create_window(550, 310, anchor="center", window=button)
+button = ctk.CTkButton(app, text="Download Video", command=download_video)
+button_window = canvas.create_window(400, 310, anchor="center", window=button)
 
 # Executar a aplicação
 app.mainloop()
