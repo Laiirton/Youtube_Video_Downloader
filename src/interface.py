8ad2d5cd65ee5496c2c32891f9d9ae8f1f73fa92
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-13 02:06:39 -0300

    chore: Update canvas size in interface.py

--- src/interface.py
+++ src/interface.py
@@ -4,13 +4,13 @@ from PIL import Image, ImageTk
 
 # Configuração da janela principal
 app = ctk.CTk()
-app.geometry("702x406")
+app.geometry("1000x600")
 app.title("YouTube Video Downloader")
 
 # Carregar e configurar a imagem de fundo
 bg_image = Image.open("./src/assets/background.jpg")
 bg_image = ImageTk.PhotoImage(bg_image)
-canvas = tk.Canvas(app, width=720, height=406)
+canvas = tk.Canvas(app, width=1000, height=600)
 canvas.pack(fill="both", expand=True)
 canvas.create_image(0, 0, image=bg_image, anchor="nw")
 
